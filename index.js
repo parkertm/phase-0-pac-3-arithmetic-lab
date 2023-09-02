21a074ec1856392712526bb81f961a44957b231d
Timothy Parker <papasforpapa@gmail.com>
2023-09-01 23:00:16 -0400

    Modifies index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(c, d) {
+    return c - d;
+}
+
+function multiply(e, f) {
+    return e * f;
+}
+
+function divide(g, h) {
+    return g / h;
+}
+let x = 10;
+function increment(x) {
+return (x += 1);
+}
+
+function decrement(x) {
+    return (x -=1);
+}
+
+n = 5.8914
+function makeInt(n) {
+    return (parseInt(n, 10));
+}
+
+function preserveDecimal(n) {
+    return (parseFloat(n));
+}
